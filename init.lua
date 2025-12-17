f2105b722a2783714042c1a5502007c93fe98d53
Victor Salcedo <vsalc@proton.me>
2025-12-17 00:27:36 -0500

    made a few changes, oh wait

---
+++ init.lua
@@ -0,0 +1,85 @@
+-- file: init.lua
+-- purpose: simple nvim config
+-- author: victor salcedo
+-- date: november 24, 2025
+
+-- essentials
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+-- quality of life
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = 'yes'
+
+vim.g.mapleader= " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", 
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local opts = {}
+local plugins = {
+  { 
+    "rebelot/kanagawa.nvim",
+    priority = 1000 
+  },
+  {
+    'nvim-telescope/telescope.nvim', 
+    tag = '0.1.6',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {
+    "nvim-treesitter/nvim-treesitter", 
+    build= ":TSUpdate"
+  },
+  {
+    'neovim/nvim-lspconfig',
+  },
+  {
+    'rust-lang/rust.vim',
+    ft = 'rust',
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^5',
+    lazy = false,
+  },
+}
+
+require("lazy").setup(plugins, opts)
+require("kanagawa").setup()
+vim.cmd.colorscheme "kanagawa"
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"lua", "rust"},
+  highlight = { enable = true },
+  indent = { enable = true }
+})
